d6f8499bd21e14d1c0485f5f2c25a16877e0c734
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-03 21:59:24 +0200

    Completed deno introduction

--- main.ts
+++ main.ts
@@ -1,5 +1,28 @@
 import { green, red, serve, v4 } from "./deps.ts";
 
 const port = 2345;
-const server = serve({ port });
 const hashes = new Set();
+
+serve(
+  (req) => {
+    const url = new URL(req.url);
+    const hash = url.pathname.slice(1);
+
+    if (hash.length > 0) {
+      if (hashes.has(hash)) {
+        console.log(green("Successfull connection"));
+        return new Response(`Welcome back!`);
+      } else {
+        console.log(red("Hash not recognized"));
+        return new Response(`Hash was not recognized, please try again`);
+      }
+    } else {
+      const newHash = v4.generate();
+      hashes.add(newHash);
+      return new Response(`Hello World!\nYour hash is: ${newHash}`);
+    }
+  },
+  { port }
+);
+
+console.log(green(`Server is accessible @ http://localhost:${port}/`));
