8ca9c7b6f2c7311c2bc3ab974f3a24bc42826dd9
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-03 23:29:46 +0200

    Reworked file structure

---
+++ PostgreSQL/postgres.ts
@@ -0,0 +1,25 @@
+import { Client, Pool } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
+
+const client = new Client({
+  user: "postgres",
+  password: "password",
+  database: "postgres",
+  hostname: "localhost",
+  port: 6543,
+});
+
+await client.connect();
+
+await client.queryObject(
+  "CREATE TABLE IF NOT EXISTS test_table (column_1 VARCHAR)"
+);
+
+await client.queryObject(
+  "INSERT INTO test_table (column_1) VALUES ('string 1'), ('string 2')"
+);
+
+const result = await client.queryObject("SELECT * FROM test_table");
+
+console.log(result);
+
+await client.end();
