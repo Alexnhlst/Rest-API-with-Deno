b1342b10edc8625d34319b8ce58e4b085a1759fb
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-04 00:40:18 +0200

    Completed sqlite section

--- SQLite/sqlite.ts
+++ SQLite/sqlite.ts
@@ -1,3 +1,14 @@
 import { DB } from "https://deno.land/x/sqlite@v3.2.1/mod.ts";
 
 const db = new DB("test.db");
+
+db.query("CREATE TABLE IF NOT EXISTS test_table (column_1 VARCHAR(100))");
+db.query("INSERT INTO test_table (column_1) VALUES ('string 1'), ('string 2')");
+
+const resultArray = [...db.query("SELECT * FROM test_table")];
+
+const resultObject = [...db.queryEntries("SELECT * FROM test_table")];
+
+console.log({ resultArray, resultObject });
+
+db.close();
