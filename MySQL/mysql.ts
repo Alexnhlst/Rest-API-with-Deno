fa7e9baf2fa66762d41dfbf648cb3c37ec732e74
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-04 00:17:15 +0200

    Added mysql query section

--- MySQL/mysql.ts
+++ MySQL/mysql.ts
@@ -5,3 +5,17 @@ const client = await new Client().connect({
   username: "root",
   db: "mysql",
 });
+
+await client.execute(
+  "CREATE TABLE IF NOT EXISTS test_table (column_1 VARCHAR(100))"
+);
+
+await client.execute(
+  "INSERT INTO test_table (column_1) VALUES ('string 1'), ('string 2')"
+);
+
+const result = await client.query("SELECT * FROM test_table");
+
+console.log(result);
+
+await client.close();
